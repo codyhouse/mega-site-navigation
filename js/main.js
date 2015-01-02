784fb783c269d1920362f7fb89b7388c3a7e67a3
claudia-romano <claudia.romano06@gmail.com>
2015-01-02 16:16:08 +0000

    Bug fixed on IE11 - .cd-overlay covering nav

--- js/main.js
+++ js/main.js
@@ -94,10 +94,12 @@ jQuery(document).ready(function($){
 			//close serach 
 			$('.cd-search').removeClass('is-visible');
 			$('.cd-search-trigger').removeClass('search-is-visible');
+			$('.cd-overlay').removeClass('search-is-visible');
 		} else {
 			//toggle search visibility
 			$('.cd-search').toggleClass('is-visible');
 			$('.cd-search-trigger').toggleClass('search-is-visible');
+			$('.cd-overlay').toggleClass('search-is-visible');
 			if($(window).width() > MqL && $('.cd-search').hasClass('is-visible')) $('.cd-search').find('input[type="search"]').focus();
 			($('.cd-search').hasClass('is-visible')) ? $('.cd-overlay').addClass('is-visible') : $('.cd-overlay').removeClass('is-visible') ;
 		}
